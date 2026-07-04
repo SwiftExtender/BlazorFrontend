9f0470361d101be8521e54d0f18bd7ef4bd8694e
Andrew Malyshev <far07156@gmail.com>
2026-07-05 02:52:12 +0300

    Explore grid

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,8 +8,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services
-    .AddCryptoClient1()
-    .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:4108/graphql"));
 
 await builder.Build().RunAsync();
