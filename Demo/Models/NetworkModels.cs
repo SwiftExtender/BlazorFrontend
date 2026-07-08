68571e6789da8a039fb312ac93b394b28089f7af
Andrew Malyshev <far07156@gmail.com>
2026-07-08 04:10:35 +0300

    components dividing

---
+++ Demo/Models/NetworkModels.cs
@@ -0,0 +1,41 @@
+ namespace Demo.Models;
+ 
+ public class Hosts
+    {
+        public List<Host> hosts { get; set; }
+    }
+    // C# Model matching the JSON structure
+    public class Host
+    {
+        public int Id { get; set; }
+        public string Ip { get; set; }
+        public string SubnetId { get; set; }
+        public string Subnet { get; set; }
+        public string Info { get; set; }
+        public List<Domain> Domains { get; set; }
+        public List<Port> Ports { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+    public class Tag {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Info { get; set; }
+    }
+    public class Port {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string? Info { get; set; }
+        public int HostId { get; set; } 
+        public string Protocol { get; set; }
+        public string? Banner { get; set; }
+    }
+    public class Domains {
+        public List<Domain> domains { get; set; }
+    }
+    public class Domain {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Info { get; set; }
+    }
